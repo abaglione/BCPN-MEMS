cad2b6003632c75c36a1bfeb8d5a58dcf679f11d
Anna Baglione <anbaglione@gmail.com>
2021-10-25 13:17:46 -0400

    realized we need to calculate metrics across all runs (i.e., AUC), so re-running everything...

--- bcpn_pipeline/models/predict.py
+++ bcpn_pipeline/models/predict.py
@@ -77,22 +77,22 @@ def gather_shap(X, method, shap_values, test_indices):
     # https://lucasramos-34338.medium.com/visualizing-variable-importance-using-shap-and-cross-validation-bd5075e9063a
 
     # Combine results from all iterations
-    all_test_indices = test_indices[0]
-    all_shap_values = np.array(shap_values[0])
+    test_indices_all = test_indices[0]
+    shap_values_all = np.array(shap_values[0])
 
     for i in range(1, len(test_indices)):
-        all_test_indices = np.concatenate((all_test_indices, test_indices[i]), axis=0)
+        test_indices_all = np.concatenate((test_indices_all, test_indices[i]), axis=0)
         
         if method == 'RF' or method == 'SVM': # classifiers with multiple outputs
-            all_shap_values = np.concatenate(
-                (all_shap_values, np.array(shap_values[i])), axis=1)
+            shap_values_all = np.concatenate(
+                (shap_values_all, np.array(shap_values[i])), axis=1)
         else:
-            all_shap_values = np.concatenate((all_shap_values, shap_values[i]), axis=0)
+            shap_values_all = np.concatenate((shap_values_all, shap_values[i]), axis=0)
 
     # Bring back variable names
-    X_test = pd.DataFrame(X.iloc[all_test_indices], columns=X.columns)
+    X_test = pd.DataFrame(X.iloc[test_indices_all], columns=X.columns)
 
-    return X_test, all_shap_values
+    return X_test, shap_values_all
 
 # Thank you to Lee Cai, who bootstrapped a similar function in a diff project
 # Modifications have been made to suit this project.
@@ -145,198 +145,181 @@ def optimize_params(X, y, groups, method, random_state):
     tune_search.fit(X.values, y.values, groups)
     return tune_search.best_estimator_
 
-
-def train_test(fs, method, clf, n_lags, random_state, 
-               optimize, importance, common_fields):
-
-    ''' Set up cross validation and AUC metrics
-        Need to be splitting at the subject level
-        Thank you, Koesmahargyo et al.! ''' 
-    
-    # Get list of indices of nominal columns for SMOTE-NC upsampling, used in train_test
-    nominal_idx = sorted([fs.df.columns.get_loc(c) for c in fs.nominal_cols])
-#     print('Nominal Indices')
-#     print(nominal_idx)
-
-    # Split into inputs and labels
-    X = fs.df[[col for col in fs.df.columns if col != fs.target_col]]
-    y = fs.df[fs.target_col]
-    
-    # Set up outer CV and AUC metric storage objects
-    cv = StratifiedGroupKFold(n_splits=5, shuffle=True, random_state=random_state)
-    auc_type = 'mean'
-    tprs = [] # Array of true positive rates
-    aucs = []# Array of AUC scores
-    mean_fpr = np.linspace(0, 1, 100)
-
-    # Begin train/test
-    
-    train_res = [] # Array of dataframes of true vs pred labels
-    test_res = [] # Array of dataframes of true vs pred labels
-    all_shap_values = list() 
-    all_test_index = list()
-
-    for train_index, test_index in cv.split(X=X, y=y, groups=X[fs.id_col]):
-        X_train, y_train = X.loc[train_index, :], y[train_index]
-        X_test, y_test = X.loc[test_index, :], y[test_index]
-
-        # Perform upsampling to handle class imbalance
-        print('Conducting upsampling with SMOTE.')
-        smote = SMOTENC(random_state=random_state, categorical_features=nominal_idx)
-        cols = X_train.columns
-        X_train_upsampled, y_train_upsampled = smote.fit_resample(X_train, y_train)
-        X_train = pd.DataFrame(X_train_upsampled, columns=cols, dtype=float)
-        
-        # Save the upsampled groups array
-        upsampled_groups = X_train[fs.id_col]
-        
-        # Drop this column from the Xs - IMPORTANT!
-        X_train.drop(columns=[fs.id_col], inplace=True)
-        X_test.drop(columns=[fs.id_col], inplace=True)
-
-        # Format y
-        y_train = pd.Series(y_train_upsampled)
-        
-        ''' Perform Scaling
-            Thank you for your guidance, @Miriam Farber
-            https://stackoverflow.com/questions/45188319/sklearn-standardscaler-can-effect-test-matrix-result
-        '''
-        print('Performing MinMax scaling.')
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        
-        X_train_scaled = scaler.fit_transform(X_train)
-        index = X_train.index
-        cols = X_train.columns
-        X_train = pd.DataFrame(X_train_scaled, index=index, columns=cols)
-        
-        X_test_scaled = scaler.fit_transform(X_test)
-        index = X_test.index
-        cols = X_test.columns
-        X_test = pd.DataFrame(X_test_scaled, index=index, columns=cols)
-
-        # Replace our default classifier clf with an optimized one
-        if optimize:
-            print('Getting optimized classifier, using gridsearch.')
-            clf = optimize_params(X=X_train, y=y_train, groups=upsampled_groups, 
-                                  method=method, random_state=random_state)
-
-        print('Training and testing.')
-        clf.fit(X_train.values, y_train.values)
-
-        # Be sure to store the training results so we can check for overfitting later
-        y_train_pred = clf.predict(X_train.values)
-        y_test_pred = clf.predict(X_test.values)
-        y_test_probas = clf.predict_proba(X_test.values)[:, 1]
-
-        # Store the AUC metrics according to the type of AUC we need (aggregate or mean)
-        if auc_type == 'agg':
-            y_test_all.append(y_test)
-            y_test_probas_all.append(y_test_probas)
-            
-        elif auc_type == 'mean':
-            # Store TPR and AUC
-            # Thank you sklearn documentation https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc_crossval.html
-            fpr, tpr, thresholds = roc_curve(y_test, y_test_probas)
-            tprs.append(interp(mean_fpr, fpr, tpr))
-            tprs[-1][0] = 0.0
-            roc_auc = auc(fpr, tpr)
-            aucs.append(roc_auc)
-        
-        # Store predicted and actual target values in dataframe
-        train_res.append(pd.DataFrame({'pred': y_train_pred, 'actual': y_train}))
-        test_res.append(pd.DataFrame({'pred': y_test_pred, 'actual': y_test}))
-
-        # Calculate feature importance while we're here, using SHAP
-        print('Calculating feature importance.')
-        if importance:
-            shap_values = calc_shap(X_train=X_train, X_test=X_test,
-                                    model=clf, method=method, random_state=random_state)
-            all_shap_values.append(shap_values)
-            all_test_index.append(test_index)
-
-    # Get and save all the shap values
-    if importance:
-        ''' Don't forget to drop the groups col and unselected feats.
-            Otherwise, we'll have issues with alignment.'''
-        
-        X_test, shap_values = gather_shap(
-            X=X.drop(columns=[fs.id_col]), method=method, 
-            shap_values=all_shap_values, test_indices=all_test_index
-        )
-
-        filename = fs.name + '_' + method + '_' + str(n_lags) + '_lags'
-        if optimize:
-            filename += '_optimized'
-        filename += '.ob'
-        
-        with open('feature_importance/X_test_' + filename, 'wb') as fp:
-            pickle.dump(X_test, fp)
-
-        with open('feature_importance/shap_' + filename, 'wb') as fp:
-            pickle.dump(shap_values, fp)
-
-    # Save all relevant stats
-    print('Calculating predictive performance metrics.')
-
-    # Get train and test results as separate dictionaries
-    train_res = pd.concat(train_res, copy=True)
-    test_res = pd.concat(test_res, copy=True)
-
-    train_perf_metrics = get_performance_metrics(train_res)
-    test_perf_metrics = get_performance_metrics(test_res)
-    
-    # Calculate AUC Metrics
-    print('Calculating AUC metrics.')
-    if auc_type == 'agg':
-        test_auc_metrics, test_tpr, test_fpr = get_agg_auc(y_test_all, y_test_probas_all)
-
-    elif auc_type == 'mean':
-        test_auc_metrics, test_tpr, test_fpr = get_mean_auc(tprs, aucs, mean_fpr)
-    
-    ''' Create a combined results and auc dictionary
-        Add only the accuracy from the training results
-          just used to ensure we aren't overfitting'''
-    
-    all_res = {
-        **{'train_accuracy': train_perf_metrics['accuracy']},
-        **{'test_' + str(k): v for k, v in test_perf_metrics.items()},
-        **{'test_' + str(k): v for k, v in test_auc_metrics.items()},
-    }
-
-    all_res.update(common_fields)
-    all_res.update({'n_features': X.shape[1], 'n_samples': X.shape[0]})
-    save_res_auc(all_res, test_tpr, test_fpr)
-#     return all_res
-
-def predict(fs, n_lags=None, classifiers=None, n_runs=5, 
+def predict(fs, n_lags=None, methods=None, n_runs=5, 
             optimize=False, importance=False, additional_fields=None):
 
-    all_results = []
-    common_fields = {'n_lags': n_lags, 'featureset': fs.name, 'target': fs.target_col}
+    common_fields = {'n_lags': n_lags, 'featureset': fs.name, 'optimized': optimize,
+                     'target': fs.target_col}
     
     if additional_fields:
         common_fields.update(additional_fields)
-
-    # Do repeated runs
-    for run in range(0, n_runs):
-
-        # If no subset of classifiers is specified, start with all default classifiers
-        if not classifiers:
-            classifiers = {
-                'LogisticR': LogisticRegression(solver='liblinear', random_state=run), 
-                'RF': RandomForestClassifier(max_depth=1, random_state=run), 
-                #'XGB': xgboost.XGBClassifier(random_state=run), 
-                'SVM': SVC(probability=True, random_state=run)
-            }
-
-        for method, clf in classifiers.items():
     
+    # If no subset of classifiers is specified, start with all default classifiers
+    if not methods:
+        methods = ['LogisticR', 'RF', 'SVM']
+
+    for method in methods:
+        tprs_all = [] # Array of true positive rates
+        aucs_all = []# Array of AUC scores
+        mean_fpr = np.linspace(0, 1, 100)
+
+        train_res_all = [] # Array of dataframes of true vs pred labels
+        test_res_all = [] # Array of dataframes of true vs pred labels
+
+        shap_values_all = list() 
+        test_indices_all = list()
+
+        # Do repeated runs
+        for run in range(0, n_runs):
+            random_state = run
+            common_fields.update({'method': method, 'run': run})
+
+            # Get the correct classifier
+            if method == 'LogisticR':
+                clf = LogisticRegression(solver='liblinear', random_state=random_state)
+            elif method == 'RF':
+                clf = RandomForestClassifier(max_depth=1, random_state=random_state)
+            elif method == 'SVM':
+                clf = SVC(probability=True, random_state=random_state)
+
+            # Do training and testing
             print('Run %i of %i for %s model.' % (run + 1, n_runs, method))
         
-            common_fields.update({'method': method, 'optimized': optimize, 'run': run})
-            train_test(fs, method=method, clf=clf, n_lags=n_lags, 
-                       random_state=run, optimize=optimize, importance=importance,
-                       common_fields = common_fields)
+            # Get list of indices of nominal columns for SMOTE-NC upsampling, used in train_test
+            nominal_idx = sorted([fs.df.columns.get_loc(c) for c in fs.nominal_cols])
+
+            # Split into inputs and labels
+            X = fs.df[[col for col in fs.df.columns if col != fs.target_col]]
+            y = fs.df[fs.target_col]
+            
+            # Set up outer CV
+            ''' Need to be splitting at the subject level
+                Thank you, Koesmahargyo et al.! ''' 
+            cv = StratifiedGroupKFold(n_splits=5, shuffle=True, random_state=random_state)
+
+            # Do prediction task
+            for train_index, test_index in cv.split(X=X, y=y, groups=X[fs.id_col]):
+                X_train, y_train = X.loc[train_index, :], y[train_index]
+                X_test, y_test = X.loc[test_index, :], y[test_index]
+
+                # Perform upsampling to handle class imbalance
+                print('Conducting upsampling with SMOTE.')
+                smote = SMOTENC(random_state=random_state, categorical_features=nominal_idx)
+                cols = X_train.columns
+                X_train_upsampled, y_train_upsampled = smote.fit_resample(X_train, y_train)
+                X_train = pd.DataFrame(X_train_upsampled, columns=cols, dtype=float)
+                
+                # Save the upsampled groups array
+                upsampled_groups = X_train[fs.id_col]
+                
+                # Drop this column from the Xs - IMPORTANT!
+                X_train.drop(columns=[fs.id_col], inplace=True)
+                X_test.drop(columns=[fs.id_col], inplace=True)
+
+                # Format y
+                y_train = pd.Series(y_train_upsampled)
+                
+                ''' Perform Scaling
+                    Thank you for your guidance, @Miriam Farber
+                    https://stackoverflow.com/questions/45188319/sklearn-standardscaler-can-effect-test-matrix-result
+                '''
+                print('Performing MinMax scaling.')
+                scaler = MinMaxScaler(feature_range=(0, 1))
+                
+                X_train_scaled = scaler.fit_transform(X_train)
+                index = X_train.index
+                cols = X_train.columns
+                X_train = pd.DataFrame(X_train_scaled, index=index, columns=cols)
+                
+                X_test_scaled = scaler.fit_transform(X_test)
+                index = X_test.index
+                cols = X_test.columns
+                X_test = pd.DataFrame(X_test_scaled, index=index, columns=cols)
+
+                # Replace our default classifier clf with an optimized one
+                if optimize:
+                    print('Getting optimized classifier using gridsearch.')
+                    clf = optimize_params(X=X_train, y=y_train, groups=upsampled_groups, 
+                                          method=method, random_state=random_state)
+
+                print('Training and testing.')
+                clf.fit(X_train.values, y_train.values)
+
+                # Be sure to store the training results so we can check for overfitting later
+                y_train_pred = clf.predict(X_train.values)
+                y_test_pred = clf.predict(X_test.values)
+                y_test_probas = clf.predict_proba(X_test.values)[:, 1]
+
+                # Store TPR and AUC
+                # Thank you sklearn documentation https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc_crossval.html
+                fpr, tpr, thresholds = roc_curve(y_test, y_test_probas)
+                tprs_all.append(interp(mean_fpr, fpr, tpr))
+                tprs_all[-1][0] = 0.0
+                roc_auc = auc(fpr, tpr)
+                aucs_all.append(roc_auc)
+            
+                # Store predicted and actual target values in dataframe
+                train_res_all.append(pd.DataFrame({'pred': y_train_pred, 'actual': y_train}))
+                test_res_all.append(pd.DataFrame({'pred': y_test_pred, 'actual': y_test}))
+
+                # Calculate feature importance while we're here, using SHAP
+                if importance:
+                    print('Calculating feature importance.')
+                    shap_values = calc_shap(X_train=X_train, X_test=X_test,
+                                            model=clf, method=method, random_state=random_state)
+                    shap_values_all.append(shap_values)
+                    test_indices_all.append(test_index)
+
+        # Get and save all the shap values
+        if importance:
+            print('Gathering feature importance across all runs.')
+
+            ''' Don't forget to drop the groups col and unselected feats.
+                Otherwise, we'll have issues with alignment.'''
+            X_test, shap_values = gather_shap(
+                X=X.drop(columns=[fs.id_col]), method=method, 
+                shap_values=shap_values_all, test_indices=test_indices_all
+            )
+
+            filename = fs.name + '_' + method + '_' + str(n_lags) + '_lags'
+            if optimize:
+                filename += '_optimized'
+            filename += '.ob'
+            
+            with open('feature_importance/X_test_' + filename, 'wb') as fp:
+                pickle.dump(X_test, fp)
+
+            with open('feature_importance/shap_' + filename, 'wb') as fp:
+                pickle.dump(shap_values, fp)
+
+        # Save all relevant stats
+        print('Calculating predictive performance metrics across all runs.')
+
+        # Get train and test results as separate dictionaries
+        train_res_all = pd.concat(train_res_all, copy=True)
+        test_res_all = pd.concat(test_res_all, copy=True)
+
+        train_perf_metrics = get_performance_metrics(train_res_all)
+        test_perf_metrics = get_performance_metrics(test_res_all)
+        
+        # Calculate AUC Metrics
+        print('Calculating AUC metrics across all runs.')
+        test_auc_metrics, test_tpr, test_fpr = get_mean_auc(tprs_all, aucs_all, mean_fpr)
+        
+        ''' Create a combined results and auc dictionary
+            Add only the accuracy from the training results
+            just used to ensure we aren't overfitting'''
+        
+        all_res = {
+            **{'train_accuracy': train_perf_metrics['accuracy']},
+            **{'test_' + str(k): v for k, v in test_perf_metrics.items()},
+            **{'test_' + str(k): v for k, v in test_auc_metrics.items()},
+        }
+
+        all_res.update(common_fields)
+        all_res.update({'n_features': X.shape[1], 'n_samples': X.shape[0]}) # OK to just take most recent X - won't change across runs
+        save_res_auc(all_res, test_tpr, test_fpr)
+        #     return all_res
 
 def tune_lags(fs):
     
@@ -373,6 +356,5 @@ def tune_lags(fs):
                 'RF': RandomForestClassifier(max_depth=max_depth, random_state=max_depth)
             }
             
-            predict(all_feats, n_lags, classifiers=classifiers,
-                    optimize=False, importance=False, n_runs=5,
+            predict(all_feats, n_lags, optimize=False, importance=False, n_runs=5,
                     additional_fields={'max_depth': max_depth})
